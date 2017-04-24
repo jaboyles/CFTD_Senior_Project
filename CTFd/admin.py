83a49bc4dc87c03d57aab34b23c3d844f33cccfa
Cameron Geehr <camgeehr@gmail.com>
2017-04-23 18:02:58 -0700

    File saves now upon uploading file

--- CTFd/admin.py
+++ CTFd/admin.py
@@ -44,9 +44,12 @@ def get_section():
     return str(sectionid)
 
 
-@admin.route('/admin/section/new', methods=['POST'])
+@admin.route('/admin/section/new', methods=['POST', 'GET'])
 @admins_only
 def new_section_from_file():
+    print("Got here")
+    if request.method == 'GET':
+        return render_template('admin/new_section.html')
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
